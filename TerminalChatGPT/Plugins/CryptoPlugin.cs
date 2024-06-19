20741cd1e872dbeb7bb2c86ce8c8ad930d466db3
stijn-castelyns <stijn@u2u.be>
2024-06-19 16:55:31 +0200

    Add project files.

---
+++ TerminalChatGPT/Plugins/CryptoPlugin.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.SemanticKernel;
+using System.ComponentModel;
+
+namespace TerminalChatGPT.Plugins;
+public class CryptoPlugin
+{
+  private IHttpClientFactory _httpClientFactory;
+  public CryptoPlugin(IHttpClientFactory httpClientFactory)
+  {
+    _httpClientFactory = httpClientFactory;
+  }
+  [KernelFunction]
+  [Description("Retrieves the current price of Bitcoin in EUR, GBP and USD")]
+  public async Task<string> GetBitcoinInfo()
+  {
+    var httpClient = _httpClientFactory.CreateClient(nameof(CryptoPlugin));
+    var bitcoinResponse = await httpClient.GetAsync("v1/bpi/currentprice.json");
+
+    string bitcoinInfo = await bitcoinResponse.Content.ReadAsStringAsync();
+    return bitcoinInfo;
+  }
+}
